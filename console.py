0e3a46b675961eaf7db24f3ab9597d7719abda97
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-09-03 11:14:51 +0100

    console_o.1

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 for the console command interpreter.
 """
 import cmd
+from ntpath import join
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
@@ -28,6 +29,7 @@ class HBNBComand(cmd.Cmd):
         "Review"
         )
 
+
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
@@ -45,9 +47,18 @@ class HBNBComand(cmd.Cmd):
         if args[0] not in HBNBComand.class_model:
             print("** class doesn't exist **")
             return True
-        if len(args) < 2 and command != 'create':
+        if len(args) < 2 and command not in ('create', 'all'):
             print("** instance id missing **")
             return True
+
+        if command  in ("show", "delete", "update"):
+            obj = storage.all()
+            key = f"{args[0]}.{args[1]}"
+            _str = obj.get(key)
+            if _str is None:
+                print("** no instance found **")
+                return True
+
         return False
 
     def do_create(self, line):
@@ -66,17 +77,75 @@ class HBNBComand(cmd.Cmd):
         """Prints string representation of instance
         based on class name and id."""
         args = line.split()
-        if HBNBComand.HBNBError(line):
+        if HBNBComand.HBNBError(line, "show"):
             return
         
-        obj = storage.all()
-        key = args[0] + '.' + args[1]
-        print(obj.get(key))
+        obj_dict = storage.all()
+        key = f"{args[0]}.{args[1]}"
+        _str = obj_dict.get(key)
+        print(_str)
 
-    def destroy(self, line):
+    def do_destroy(self, line):
         """Deletes an instance based on class name and id."""
-        pass
+        args = line.split()
+        if HBNBComand.HBNBError(line,  "delete"):
+            return
+        key = f"{args[0]}.{args[1]}"
+        obj = storage.all()
+        del (obj[key])
+        storage.save()
 
+    def do_all(self, line):
+        """Prints all sting representation based or not on the class name."""
+        args = line.split()
+        _str = []
+        obj = storage.all()
+
+        if args:
+            if HBNBComand.HBNBError(line, 'all'):
+                return   
+            key = args[0]
+            for item in obj:
+                if key in item:
+                    _str.append(str(obj.get(item)))
+        
+        else:  
+            for item in obj:
+                _str.append(str(obj.get(item)))
+        ", ".join(_str)
+        print(_str)
+    
+    def do_update(self, line):
+        """Updates an instance based on the class name and id\
+            by adding or updating attribute"""
+        args = line.split()
+        if HBNBComand.HBNBError(line, "update"):
+            return
+        if  len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        print(f"key/value -> {args[2]}: {args[3]}")
+
+        obj_dict = storage.all()
+        key = f"{args[0]}.{args[1]}"
+        print(key)
+        item = obj_dict.get(key)
+        print(item)
+        # print(type(item))
+        # eval(item)[args[2]] = args[3]
+        # storage.save()
+        #  obj = eval(key)
+        setattr(item, args[2], args[3])
+        item.save()
+        #obj[args[2]] = args[3]
+        storage.save()
+
+
+        pass
+        
     def emptyline(self):
         pass
 
