2da2c78c6645850e2eacb0bb82392fe79fdd5a3e
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-11-22 11:43:33 +0100

    update console functions

--- models/state.py
+++ models/state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a class State."""
+"""This module defines a class City."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represents a State class that inherits from BaseModel."""
+    """Represents a City class that inherits BaseModel."""
+    state_id = ""
     name = ""
